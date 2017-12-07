70131d144e819a79c388c84fd58748c4602f82e6
ryh95 <1971990184@qq.com>
2017-12-07 10:41:20 +0800

    update config

--- config.py
+++ config.py
@@ -4,22 +4,24 @@ import os
 
 base_dir = os.path.dirname(os.path.realpath(__file__))
 data_dir = pjoin(base_dir,'data/squad')
-train_answer_span_file = pjoin(data_dir,'train.span_sorted')
-train_question_file = pjoin(data_dir, 'train.ids.question_sorted')
-train_context_file = pjoin(data_dir, 'train.ids.context_sorted')
-val_answer_span_file = pjoin(data_dir,'val.span')
-val_question_file = pjoin(data_dir, 'val.ids.question')
-val_context_file = pjoin(data_dir, 'val.ids.context')
+ckpt_dir = pjoin(base_dir,'data/ckpt')
+train_answer_span_file = pjoin(data_dir,'train.span_sorted_1000')
+train_question_file = pjoin(data_dir, 'train.ids.question_sorted_1000')
+train_context_file = pjoin(data_dir, 'train.ids.context_sorted_1000')
+val_answer_span_file = pjoin(data_dir,'val.span_1000')
+val_question_file = pjoin(data_dir, 'val.ids.question_1000')
+val_context_file = pjoin(data_dir, 'val.ids.context_1000')
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='PyTorch TreeLSTM for Sentence Similarity on Dependency Trees')
+    parser = argparse.ArgumentParser(description='PyTorch BiDAF')
     parser.add_argument("--lr",default=0.0005,help='learning rate')
     parser.add_argument("--max_gradient_norm",default=10.0,help='Clip gradients to this norm.')
 
     parser.add_argument("--dropout", default=0.20, help="Fraction of units randomly dropped on non-recurrent connections.")
-    parser.add_argument("--batch_size", default=24, help="Batch size to use during training.")
+    parser.add_argument("--batch_size", default=5, help="Batch size to use during training.")
     parser.add_argument("--epochs", default=25, help="Number of epochs to train.")
     parser.add_argument("--encoder_state_size", default=100, help="Size of each encoder model layer.")
+    parser.add_argument("--encoder_bidirectional",default=True)
     parser.add_argument("--decoder_state_size", default=100, help="Size of each decoder model layer.")
     parser.add_argument("--output_size", default=750, help="The output size of your model.")
     parser.add_argument("--embedding_size", default=100, help="Size of the pretrained vocabulary.")
@@ -29,18 +31,16 @@ def parse_args():
     parser.add_argument("--train_answer_span_file",default=train_answer_span_file)
     parser.add_argument("--train_question_file",default=train_question_file)
     parser.add_argument("--train_context_file",default=train_context_file)
-    parser.add_argument("--train_dir", default="train",
-                               help="Training directory to save the model parameters (default: ./train).")
+    parser.add_argument("--ckpt_dir", default=ckpt_dir,
+                               help="checkpoint directory to save the model parameters")
+    parser.add_argument("--expname",default="debug")
     parser.add_argument("--val_answer_span_file", default=val_answer_span_file)
     parser.add_argument("--val_question_file", default=val_question_file)
     parser.add_argument("--val_context_file", default=val_context_file)
 
-    parser.add_argument("--load_train_dir", default="",
-                               help="Training directory to load model parameters from to resume training (default: {train_dir}).")
     parser.add_argument("--log_dir", default="log", help="Path to store log and flag files (default: ./log)")
     parser.add_argument("--optimizer", default="adam", help="adam / sgd")
     parser.add_argument("--print_every", default=1, help="How many iterations to do per print.")
-    parser.add_argument("--keep", default=0, help="How many checkpoints to keep, 0 indicates keep all.")
     parser.add_argument("--vocab_path", default="",
                                help="Path to vocab file (default: ./data/squad/vocab.dat)")
     parser.add_argument("--embed_path", default="",
@@ -53,7 +53,7 @@ def parse_args():
     parser.add_argument("--decoder_hidden_size", default=100, help="Number of decoder_hidden_size.")
     parser.add_argument("--QA_ENCODER_SHARE", default=True,help= "QA_ENCODER_SHARE weights.")
     parser.add_argument("--tensorboard", default=False, help="Write tensorboard log or not.")
-    parser.add_argument("--RE_TRAIN_EMBED", default=False, help="Max length of the context (default: 400)")
+    parser.add_argument("--RE_TRAIN_EMBED", default=True, help="Max length of the context (default: 400)")
     parser.add_argument("--debug_train_samples", default=None, help="number of samples for debug (default: None)")
     parser.add_argument("--ema_weight_decay", default=0.999, help="exponential decay for moving averages ")
     parser.add_argument("--evaluate_sample_size", default=400, help="number of samples for evaluation (default: 400)")
